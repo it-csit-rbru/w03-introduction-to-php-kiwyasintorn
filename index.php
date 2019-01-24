04cf83172f5a0008a78cf6a4c2b203a7d083d8d3
Yasintorn Rattananimit <6014421004@rbru.ac.th>
2019-01-24 16:00:44 +0700

    v1.02

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $email = "6014421004@rbru.ac.th";
+    $name = "Yasintorn Rattananimit";
+    $url = "stu2.rbru.ac.th/~s6014421004";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       echo "<h1>$email</h1>";
+       echo "<h2>$name</h2>";
+       echo "<a href=\"$url\" > $name</a>";
+    ?>
+</body>
+</html>
